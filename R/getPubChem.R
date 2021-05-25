3add324b2d47edfccd564ae931c86d8b0333395b
ChristopherEeles <christopher.eeles@outlook.com>
2021-05-25 16:17:17 -0400

    Add: updated docs for getPubChem function

--- R/getPubChem.R
+++ R/getPubChem.R
@@ -43,7 +43,7 @@
 #' assay domain <namespace> = aid | listkey | type/<assay type> | sourceall/<source name> | target/<assay target> | activity/<activity column name>
 #'
 #' Complete documentation for valid input specifications can be found at:
-#' https://pubchemdocs.ncbi.nlm.nih.gov/pug-rest$_Toc494865554
+#' https://pubchemdocs.ncbi.nlm.nih.gov/pug-rest$_Toc494865556
 #' 
 #' ### Operation
 #'
@@ -63,7 +63,7 @@
 #' assay domain <operation specification> = record | concise | aids | sids | cids | description | targets/<target type> | <doseresponse> | summary | classification
 #' 
 #' Complete documentation for valid operations can be found at:
-#' https://pubchemdocs.ncbi.nlm.nih.gov/pug-rest$_Toc494865554
+#' https://pubchemdocs.ncbi.nlm.nih.gov/pug-rest$_Toc494865557
 #'
 #' ### Output
 #' 
@@ -74,7 +74,7 @@
 #' <output specification> = XML | ASNT | ASNB | JSON | JSONP [ ?callback=<callback name> ] | SDF | CSV | PNG | TXT
 #'
 #' Complete documentation for valid output formats can be found at:
-#' https://pubchemdocs.ncbi.nlm.nih.gov/pug-rest$_Toc494865554
+#' https://pubchemdocs.ncbi.nlm.nih.gov/pug-rest$_Toc494865558
 #' 
 #'
 #' @param id The identifier for REST API query. This should be a valid compound
@@ -84,35 +84,59 @@
 #'   `vector`, the ids are parsed to a string with ids separated by
 #'   commas. This parameter forms the '<identifiers>' part of the Input 
 #'   portion of the URL path: '<domain>/<namespace>/<identifiers>'.
-#' @param domain Which records in PubChem are the subject of the query? Usually,
-#'   this will be one of 'substance', 'compound', or 'assay'. For more advanced
-#'   options see the PubChem PUG REST API documentation. This forms the 
-#'   '<domain>' part of the Input portion of the URL path.
-#' @param namespace What kind of identifiers are in `id`. These options
-#'   are specific to the selected domain. For the 'compound' domian, common
+#' @param domain Which records in PubChem are the subject of the query? Default 
+#'   is 'compound'. Usually, this will be one of 'substance', 'compound', or 
+#'   'assay'. For more advanced options see the PubChem PUG REST API 
+#'   documentation. This forms the '<domain>' part of the Input portion of the 
+#'   URL path.
+#' @param namespace What kind of identifiers are in `id`? These options
+#'   are specific to the selected domain. For the 'compound' domain, common
 #'   options are 'cid', 'name', 'smiles', 'inchi', or 'formula'. For the 
 #'   'substance' domain common options are 'sid' or 'name'. For the 
 #'   'assay' domain common options are 'aid' and 'target/<assay_target>' where
 #'   '<assay_target>' is one of 'gi', 'proteinname', 'geneid', 'genesymbol' or
 #'   'accession'. For all options please see the PubChem PUG REST API 
 #'   documentation. This parameter makes up the '<namespace>' part of the
-#'   input portion of the URL path.
-#' @param operation
-#' @param output
+#'   Input portion of the URL path.
+#' @param operation What kind of data to return for the specified input
+#'   portion of the URL path? If excluded this defaults to return the entire
+#'   record for that specified input. Options are domain specific and details
+#'   are available in the PubChem PUG REST API documentation. Options valid for
+#'   'compound', 'substance' and 'assay' domains are 'record', 'aids', 'sids',
+#'   'aids', 'description' and 'classification'. Common options for the 
+#'   'compound' domain are 'assaysummary', 'conformers' and 
+#'   'property/<properties>' where '<properties>' is a comma separated list 
+#'   property tags; valid tags can be found at 
+#'   https://pubchemdocs.ncbi.nlm.nih.gov/pug-rest$_Toc494865565. 
+#'   Common options for the 'substance' domain are 'assaysummary'.
+#'   Common options for the 'assay' domain include 'concise', 'summary' and
+#'   'targets/<target type>' where '<target type>' is one of 'ProteinGI', 
+#'   'ProteinName', 'GeneID' or 'GeneSymbol'.
+#' @param output What format should the data be returned in? Default is 'JSON'.
+#'   other common options include 'XML', 'CSV' and 'TXT'. For a complete list of 
+#'   output format options, plase see the PubChem PUG REST API documentation.
 #' @param ... Force subsequent arguments to be named. Not used.
-#' @param url The URL of the PubChem REST API.
-#' @param output_operation 
+#' @param url The URL of the PubChem REST API. Probably don't change this.
+#' @param operation_options Further optional arguments for the selected operation.
+#'   this is specific to the selected operation. This is appended as a string
+#'   after '?' at the end of the query. See 
+#'   https://pubchemdocs.ncbi.nlm.nih.gov/pug-rest$_Toc494865565 for details.
 #'
 #' @return A `httr::response` object with the results of the GET request.
 #'
 #' @seealso [httr::GET], [queryPubChem]
+#'
+#' @references
+#' Kim S, Thiessen PA, Cheng T, Yu B, Bolton EE. An update on PUG-REST: RESTful interface for programmatic access to PubChem. Nucleic Acids Res. 2018 July 2; 46(W1):W563-570. doi:10.1093/nar/gky294.
+#' Kim S, Thiessen PA, Bolton EE, Bryant SH. PUG-SOAP and PUG-REST: web services for programmatic access to chemical information in PubChem. Nucleic Acids Res. 2015 Jul 1; 43(W1):W605-W611. doi: 10.1093/nar/gkv396. 
+#' Kim S, Thiessen PA, Bolton EE. Programmatic Retrieval of Small Molecule Information from PubChem Using PUG-REST. In Kutchukian PS, ed. Chemical Biology Informatics and Modeling. Methods in Pharmacology and Toxicology. New York, NY: Humana Press, 2018, pp. 1-24. doi:10.1007/7653_2018_30.
 #'
 #' @md
 #' @importFrom httr GET
 #' @export
 getPubChem <- function(id, domain='compound', namespace='cid', operation='',
     output='JSON', ..., url='https://pubchem.ncbi.nlm.nih.gov/rest/pug',
-    output_operation='')
+    operation_options='')
 {
     # handle list or vector inputs for id
     if (length(id) > 1) id <- paste0(na.omit(id), collapse=',')
@@ -192,7 +216,7 @@ querySynonymsFromName <- function(drugNames) {
         results[[drug]] <- content(
             getPubChem(
                 id=drug, 
-                identifier='Name',
+                namespace='Name',
                 operation='synonyms'),
             'parsed'
         )
@@ -214,8 +238,8 @@ if (sys.nframe() == 0) {
     GDSC <- readRDS(list.files('../PSets', pattern = 'GDSC.*v2.*', full.names=TRUE))
     drugInfo <- drugInfo(GDSC)
 
-    result <- queryPubChem(id=drugInfo$cid, identifier='cid',
-        operation='aids', filter='aids_type=active')
+    result <- queryPubChem(id=drugInfo$cid, namespace='cid',
+        operation='aids', operation_options='aids_type=active')
 
     result <- querySynonymsFromName(drugs)
     unlist_result <- lapply(result, unlist)
