34b1c5591d52ec904516945404639d72289e8c86
ChristopherEeles <christopher.eeles@outlook.com>
2022-03-22 18:44:32 -0400

    Update: add missing manual pages

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@
         envir=asNamespace(packagename))
 
     # old implementation of proxies
-    response <- httr::GET('http://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeoust=10000&country=all&ssl=no&anonymity=all&simplified=true')
-    proxy <- AnnotationGx:::parse_text_response_to_datatable(response)
-    data.table::fwrite(proxy, file=file.path(tempdir(), 'proxy.csv'))
+    # response <- httr::GET('http://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeoust=10000&country=all&ssl=no&anonymity=all&simplified=true')
+    # proxy <- AnnotationGx:::parse_text_response_to_datatable(response)
+    # data.table::fwrite(proxy, file=file.path(tempdir(), 'proxy.csv'))
 }
