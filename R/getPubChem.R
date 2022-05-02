4f8d6a775cea82369bd9a9bcb55aa86c66982437
ChristopherEeles <christopher.eeles@outlook.com>
2022-05-02 19:49:37 +0000

    Merge with remote

--- R/getPubChem.R
+++ R/getPubChem.R
@@ -339,7 +339,29 @@ queryPubChem <- function(id, domain='compound', namespace='cid', operation=NA,
     return(queryRes)
 }
 
-
+#' Parse a JSON into a list
+#'
+#' @param response A `response` object as returned by `httr::GET`
+#' @param as A `character` vector indicating the return type. Options are 'raw',
+#    'text' or 'parsed'. Default is 'text'.
+#' @param ... Additional arguments to the `httr::content` function.
+#'
+#' @seelalso [httr::content]
+#'
+#' @md
+#' @importFrom jsonlite fromJSON
+#' @importFrom httr content
+#' @export
+parseJSON <- function(response, ..., encoding='UTF-8', query_only=FALSE) {
+    if (isTRUE(query_only)) return(response)
+    tryCatch({
+        fromJSON(content(response, ..., as='text', type='JSON',
+            encoding=encoding))
+    },
+    error=function(e) {
+        fromJSON(content(response, ..., type='JSON', encoding=encoding))
+    })
+}
 
 #' Query the PubChem REST API, with the result automatically converted from
 #'   JSON to a list. This only works when `output='JSON'` in `getRequestPubChem`.
