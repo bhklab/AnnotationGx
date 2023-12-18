83e167e73144063b43f7456d61341b68c7aeaa09
Jermiah <jermiahjoseph@gmail.com>
2023-12-18 17:17:40 +0000

    fix some errors in documentation and modify functions across the baord.

--- R/getPubChem-helpers.R
+++ R/getPubChem-helpers.R
@@ -1,24 +1,36 @@
-#' Checks to see if the PubChem query is exceeding the throttling limit
-#' @param response `httr::response`
-#' @param throttleMessage `logical` whether to print the throttling message
-#' @return `logical` whether the query is throttled
+#' Checks the throttling status of a request and sleeps if necessary
+#'
+#' This function checks the throttling status of a request by analyzing the
+#' "x-throttling-control" header in the response. If the throttling status
+#' indicates that the request is being throttled, the function sleeps for a
+#' certain amount of time based on the percentage of throttling. The sleep time
+#' is determined as follows:
+#' - If the percentage of throttling is greater than 50%, the function sleeps
+#'   for 30 seconds.
+#' - If the percentage of throttling is greater than 30%, the function sleeps
+#'   for 20 seconds.
+#' - If the percentage of throttling is greater than 15%, the function sleeps
+#'   for 15 seconds.
+#' - Otherwise, the function sleeps for the same amount of time as the
+#'   percentage of throttling.
+#'
+#' @param result The result of the request
+#' @param throttleMessage Logical indicating whether to print the throttling
+#'   message
+#' @return Logical indicating whether the request was throttled
 .checkThrottlingStatus <- function(result, throttleMessage = FALSE){
     message <- headers(result)$`x-throttling-control`
 
     if (throttleMessage == TRUE){
         message(message)
     }
-    # Request Count status: Green (5%), Request Time status: Green (0%), Service status: Green (30%)
 
-
-    matches <- regmatches(message, gregexpr("\\((.*?)%\\)", message))  # Extracts text within parentheses
+    matches <- regmatches(message, gregexpr("\\((.*?)%\\)", message))
     percentages <- as.numeric(gsub("\\(|%|\\)", "", unlist(matches[1:3])))
     percentage <- max(percentages[1:3])
-    # percentage <- ifelse(percentages[3] > 75, percentages[3], max(percentages[1:2]))
     sleep_time <- ifelse(percentage > 50, 30, ifelse(percentage > 30, 20, 15))
 
     if (percentage > 15) {
-        # message(paste0("Sleeping for ", sleep_time, " seconds"))
         Sys.sleep(sleep_time)
     } else {
         Sys.sleep(percentage)
@@ -28,60 +40,74 @@
 }
 
 #' simple checkThrottling Status by a default response 
-#' @return `logical` whether the query is throttled
+#' 
+#' This function checks the throttling status of a PubChem query and returns 
+#' a logical value indicating whether the query is throttled.
+#' 
+#' @param sleep A logical value indicating whether to sleep before checking 
+#' the throttling status.
+#' @return A logical value indicating whether the query is throttled.
 #' @export
+#' 
+#' @examples
+#' getPubChemStatus()
+#' getPubChemStatus(sleep = TRUE)
+#' 
+#' @keywords internal
 getPubChemStatus <- function(sleep = FALSE){
-    response <- httr::GET("https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/Aspirin/cids/JSON")
+    response <- httr::GET(
+        "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/Aspirin/cids/JSON")
     msg <- headers(response)$`x-throttling-control`
-    matches <- regmatches(msg, gregexpr("\\((.*?)%\\)", msg))  # Extracts text within parentheses
+    # Extracts text within parentheses
+    matches <- regmatches(msg, gregexpr("\\((.*?)%\\)", msg))  
     if(sleep) .checkThrottlingStatus(response)
     message(msg)
 }
 
 
-# -----------------------------
-# getPubChemAnnotations Helpers
-
-#' @importFrom data.table data.table as.data.table merge.data.table last rbindlist
-.parseATCannotations <- function(DT) {
-    dataL <- DT$Data
-    names(dataL) <- DT$SourceID
-    dataDT <- rbindlist(dataL, fill=TRUE, use.names=TRUE, idcol='SourceID')
-    dataDT[, ATC_code := unlist(lapply(Value.StringWithMarkup,
-        function(x) last(x)[[1]]))]
-    annotationDT <- merge.data.table(
-        dataDT[, .(SourceID, ATC_code)],
-        DT[, .(SourceName, SourceID, LinkedRecords)],
-        by='SourceID',
-        allow.cartesian=TRUE
-    )
-    DT <- annotationDT[, .(CID=unlist(LinkedRecords)),
-        by=.(SourceName, SourceID, ATC_code)]
-    DT <- unique(DT)
-    return(DT)
-}
-
-#' @importFrom data.table data.table as.data.table merge.data.table last rbindlist
-.parseDILIannotations <- function(DT) {
-    dataL <- DT$Data
-    names(dataL) <- DT$SourceID
-    dataL <- lapply(dataL, FUN=`[`, i=Name %like% 'DILI')
-    dataDT <- rbindlist(dataL, fill=TRUE, use.names=TRUE, idcol='SourceID')
-    dataDT[, DILI := unlist(Value.StringWithMarkup)]
-    annotationDT <- merge.data.table(
-        dataDT[, .(SourceID, DILI)],
-        DT[, .(SourceID, SourceName, Name, LinkedRecords.CID,
-            LinkedRecords.SID)],
-        by='SourceID',
-        allow.cartesian=TRUE)
-    DT <- 
-    annotationDT[, 
-        .(CID=unlist(LinkedRecords.CID), SID=unlist(LinkedRecords.SID)),
-        by=.(SourceName, SourceID, Name, DILI)]
-    return(DT)
-}
+# # -----------------------------
+# # getPubChemAnnotations Helpers
 
 # #' @importFrom data.table data.table as.data.table merge.data.table last rbindlist
+# .parseATCannotations <- function(DT) {
+#     dataL <- DT$Data
+#     names(dataL) <- DT$SourceID
+#     dataDT <- rbindlist(dataL, fill=TRUE, use.names=TRUE, idcol='SourceID')
+#     dataDT[, ATC_code := unlist(lapply(Value.StringWithMarkup,
+#         function(x) last(x)[[1]]))]
+#     annotationDT <- merge.data.table(
+#         dataDT[, .(SourceID, ATC_code)],
+#         DT[, .(SourceName, SourceID, LinkedRecords)],
+#         by='SourceID',
+#         allow.cartesian=TRUE
+#     )
+#     DT <- annotationDT[, .(CID=unlist(LinkedRecords)),
+#         by=.(SourceName, SourceID, ATC_code)]
+#     DT <- unique(DT)
+#     return(DT)
+# }
+
+# #' @importFrom data.table data.table as.data.table merge.data.table last rbindlist
+# .parseDILIannotations <- function(DT) {
+#     dataL <- DT$Data
+#     names(dataL) <- DT$SourceID
+#     dataL <- lapply(dataL, FUN=`[`, i=Name %like% 'DILI')
+#     dataDT <- rbindlist(dataL, fill=TRUE, use.names=TRUE, idcol='SourceID')
+#     dataDT[, DILI := unlist(Value.StringWithMarkup)]
+#     annotationDT <- merge.data.table(
+#         dataDT[, .(SourceID, DILI)],
+#         DT[, .(SourceID, SourceName, Name, LinkedRecords.CID,
+#             LinkedRecords.SID)],
+#         by='SourceID',
+#         allow.cartesian=TRUE)
+#     DT <- 
+#     annotationDT[, 
+#         .(CID=unlist(LinkedRecords.CID), SID=unlist(LinkedRecords.SID)),
+#         by=.(SourceName, SourceID, Name, DILI)]
+#     return(DT)
+# }
+
+# # #' @importFrom data.table data.table as.data.table merge.data.table last rbindlist
 # .parseNSCannotations <- function(DT) {
 #     DT[, NSC := unlist(lapply(Data, `[[`, i=4))]
 #     annotationDT <- DT[,
