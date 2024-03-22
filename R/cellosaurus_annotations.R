a7f4c7d010397b8d71b7baaba3f71f2e352fdcdc
Jermiah <jermiahjoseph98@gmail.com>
2024-03-22 19:43:45 +0000

    update functions and bump version

--- R/cellosaurus_annotations.R
+++ R/cellosaurus_annotations.R
@@ -2,34 +2,46 @@
 #'
 #' This function takes a Cellosaurus accession and returns annotations for the cell line.
 #'
-#' @param cellosaurus_accession The Cellosaurus accession to annotate.
+#' @param accessions The Cellosaurus accession to annotate.
 #' @param to A character vector specifying the types of annotations to retrieve. Possible values include "id", "ac", "hi", "sy", "ca", "sx", "ag", "di", "derived-from-site", "misspelling", and "dt".
 #'
 #' @return A data frame containing the annotations for the cell line.
 #'
 #' @examples
-#' annotateCellACcession("CVCL_0031", to = c("id", "ac", "hi", "sy"))
+#' annotateCellAccession("CVCL_0031")
+#' annotateCellAccession("CVCL_0031", to = c("id", "ac", "hi", "sy"))
 #'
 #' @export
-annotateCellACcession <- function(
-    cellosaurus_accession,
+annotateCellAccession <- function(
+    accessions,
     to = c("id", "ac", "hi", "sy", "ca", "sx", "ag", "di", "derived-from-site", "misspelling", "dt")
     )
 {
 
-    resp <- .build_cellosaurus_request(
-            query = cellosaurus_accession,
+    requests <- parallel::mclapply(accessions, function(accession) {
+        .build_cellosaurus_request(
+            query = accession,
             to = to,
             numResults = 1,
             apiResource = "search/cell-line",
             output = "TXT",
             sort = NULL,
             query_only = FALSE
-        )  |> .perform_request() 
-    x  <- .parse_cellosaurus_lines(resp)[[1]]
-    dt <- .processEntry(x) |>
-        .formatSynonyms()
-    dt
+        )
+    })
+        
+    responses <- .perform_request_parallel(requests)
+    names(responses) <- accessions
+    responses_dt <- parallel::mclapply(accessions,function(name) {
+        resp <- responses[[name]]
+        .parse_cellosaurus_lines(resp) |> 
+            unlist(recursive = FALSE) |> 
+            .processEntry() |>
+            .formatSynonyms()
+    }
+    ) |> data.table::rbindlist(fill = TRUE)
+
+    responses_dt
 
 }
 
