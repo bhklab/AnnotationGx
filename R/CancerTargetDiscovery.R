7691f4c7a0b7947dbca0d8a33aca964cfa9c1929
Jermiah <jermiahjoseph98@gmail.com>
2025-03-27 14:00:18 +0000

    fix: remove deprecated mapCompound2CTD function and related documentation

--- R/CancerTargetDiscovery.R
+++ R/CancerTargetDiscovery.R
@@ -1,93 +1,96 @@
-# CancerTargetDiscoveryDevelopment Functions
+# NOTE: This file is deprecated until the Cancer Target Discovery and Development (CTD2) works again
+# as of today, the https://ctd2-dashboard.nci.nih.gov/ doesnt work at all
 
-#' Map Compound to CTD
-#'
-#' This function maps a drug compound to the Cancer Target Discovery (CTD) database.
-#' It retrieves information about the compound from the CTD database and returns the results as a data table.
-#'
-#' @param compounds A character vector of drug compounds to map to the CTD database.
-#' @param base_url The base URL of the CTD API. Default is "https://ctd2-dashboard.nci.nih.gov/dashboard/get".
-#' @param endpoint The API endpoint for the compound mapping. Default is "compound".
-#' @param nParallel The number of parallel processes to use. Default is one less than the number of available cores.
-#' @param raw Logical indicating whether to return the raw response from the API. Default is FALSE.
-#' @param query_only Logical indicating whether to only return the API request URL without making the actual request. Default is FALSE.
-#'
-#' @return A data table containing the mapped information for the drug compound.
-#' If the API request fails, a data table with the drug compound name will be returned.
-#' If \code{raw} is set to TRUE, the raw response from the API will be returned.
-#'
-#' @export
-mapCompound2CTD <- function(
-    compounds,
-    base_url = "https://ctd2-dashboard.nci.nih.gov/dashboard/get", 
-    endpoint = "compound",
-    nParallel = parallel::detectCores() - 1,
-    raw = FALSE,
-    query_only = FALSE
-) {
-    # March 06 2025:
-    # this function is deprecated until
-    # the Cancer Target Discovery and Development (CTD2) works again
-    # as of today, the https://ctd2-dashboard.nci.nih.gov/ doesnt work at all
-    # we will raise an error for now
-    errmsg <- "The CTD2 API is currently not working. Please check the status of the API at https://ctd2-dashboard.nci.nih.gov/"
-    stop(errmsg)
 
-    funContext <- .funContext("mapCompound2CTD")
-    
-    # Check input types
-    checkmate::assert_character(compounds)
-    checkmate::assert_character(base_url)
-    checkmate::assert_character(endpoint)
-    checkmate::assert_logical(raw)
-    checkmate::assert_logical(query_only)
+# # CancerTargetDiscoveryDevelopment Functions
 
-    .info(funContext, 
-        sprintf("Creating requests for %s compounds", length(compounds)))
+# #' Map Compound to CTD
+# #'
+# #' This function maps a drug compound to the Cancer Target Discovery (CTD) database.
+# #' It retrieves information about the compound from the CTD database and returns the results as a data table.
+# #'
+# #' @param compounds A character vector of drug compounds to map to the CTD database.
+# #' @param base_url The base URL of the CTD API. Default is "https://ctd2-dashboard.nci.nih.gov/dashboard/get".
+# #' @param endpoint The API endpoint for the compound mapping. Default is "compound".
+# #' @param nParallel The number of parallel processes to use. Default is one less than the number of available cores.
+# #' @param raw Logical indicating whether to return the raw response from the API. Default is FALSE.
+# #' @param query_only Logical indicating whether to only return the API request URL without making the actual request. Default is FALSE.
+# #'
+# #' @return A data table containing the mapped information for the drug compound.
+# #' If the API request fails, a data table with the drug compound name will be returned.
+# #' If \code{raw} is set to TRUE, the raw response from the API will be returned.
+# #'
+# #' @export
+# mapCompound2CTD <- function(
+#     compounds,
+#     base_url = "https://ctd2-dashboard.nci.nih.gov/dashboard/get",
+#     endpoint = "compound",
+#     nParallel = parallel::detectCores() - 1,
+#     raw = FALSE,
+#     query_only = FALSE
+# ) {
+#     # March 06 2025:
+#     # this function is deprecated until
+#     # the Cancer Target Discovery and Development (CTD2) works again
+#     # as of today, the https://ctd2-dashboard.nci.nih.gov/ doesnt work at all
+#     # we will raise an error for now
+#     errmsg <- "The CTD2 API is currently not working. Please check the status of the API at https://ctd2-dashboard.nci.nih.gov/"
+#     stop(errmsg)
 
-    requests <- parallel::mclapply(compounds, function(compound){
-        compound <- gsub(" ", "-", compound)
+#     funContext <- .funContext("mapCompound2CTD")
 
-        .buildURL(base_url, endpoint, compound) |>
-            .build_request()
-    })
-    if(query_only) return(requests)
+#     # Check input types
+#     checkmate::assert_character(compounds)
+#     checkmate::assert_character(base_url)
+#     checkmate::assert_character(endpoint)
+#     checkmate::assert_logical(raw)
+#     checkmate::assert_logical(query_only)
 
-    .info(funContext, "Performing requests w/", nParallel, "parallel processes..")
-    resps <- .perform_request_parallel(requests)
-    names(resps) <- compounds
+#     .info(funContext,
+#         sprintf("Creating requests for %s compounds", length(compounds)))
 
-    results <- parallel::mclapply(
-        names(resps) , 
-        function(compound){
-            resp <- resps[[compound]]
+#     requests <- parallel::mclapply(compounds, function(compound){
+#         compound <- gsub(" ", "-", compound)
 
-            if(all(class(resp) != "httr2_response") || resp$status_code != 200){
-                dt <- data.table::data.table(
-                    displayName = compound)
-                return(dt)
-            }
+#         .buildURL(base_url, endpoint, compound) |>
+#             .build_request()
+#     })
+#     if(query_only) return(requests)
 
-            resp <- .parse_resp_json(resp)
-            if(raw) return(resp)
+#     .info(funContext, "Performing requests w/", nParallel, "parallel processes..")
+#     resps <- .perform_request_parallel(requests)
+#     names(resps) <- compounds
 
-            original_dt <- .asDT(resp$xrefs)[, c("databaseId", "databaseName")]
-            original_dt[, "displayName" := resp$displayName]
+#     results <- parallel::mclapply(
+#         names(resps) ,
+#         function(compound){
+#             resp <- resps[[compound]]
 
-            dt <- data.table::dcast(
-                    original_dt, 
-                    formula = displayName ~ databaseName, 
-                    value.var = "databaseId"
-                )
-            return(dt)
-        }, 
-        mc.cores = nParallel    
-    )
-    if(raw) {
-        names(results) <- compounds
-        return(results)
-    }
+#             if(all(class(resp) != "httr2_response") || resp$status_code != 200){
+#                 dt <- data.table::data.table(
+#                     displayName = compound)
+#                 return(dt)
+#             }
 
-    return(data.table::rbindlist(results, fill = TRUE))
-}
+#             resp <- .parse_resp_json(resp)
+#             if(raw) return(resp)
 
+#             original_dt <- .asDT(resp$xrefs)[, c("databaseId", "databaseName")]
+#             original_dt[, "displayName" := resp$displayName]
+
+#             dt <- data.table::dcast(
+#                     original_dt,
+#                     formula = displayName ~ databaseName,
+#                     value.var = "databaseId"
+#                 )
+#             return(dt)
+#         },
+#         mc.cores = nParallel
+#     )
+#     if(raw) {
+#         names(results) <- compounds
+#         return(results)
+#     }
+
+#     return(data.table::rbindlist(results, fill = TRUE))
+# }
--- tests/testthat/test_CancerTargetDiscovery.R
+++ tests/testthat/test_CancerTargetDiscovery.R
@@ -1,28 +1,28 @@
-library(AnnotationGx)
-library(testthat)
-library(checkmate)
+# library(AnnotationGx)
+# library(testthat)
+# library(checkmate)
 
-test_that("mapCompound2CTD returns expected results", {
-  expect_error(mapCompound2CTD("Invalid Compound", nParallel = 1))
-  # deprecated for now:
-  # # Test case 1: Single compound mapping
-  # compounds <- c("Bax channel blocker")
-  # result <- mapCompound2CTD(compounds, nParallel = 1)
-  # expect_true("displayName" %in% names(result))
-  # expect_true("PUBCHEM" %in% names(result))
+# test_that("mapCompound2CTD returns expected results", {
+#   expect_error(mapCompound2CTD("Invalid Compound", nParallel = 1))
+#   # deprecated for now:
+#   # # Test case 1: Single compound mapping
+#   # compounds <- c("Bax channel blocker")
+#   # result <- mapCompound2CTD(compounds, nParallel = 1)
+#   # expect_true("displayName" %in% names(result))
+#   # expect_true("PUBCHEM" %in% names(result))
 
-  # result2 <- mapCompound2CTD(compounds, nParallel = 1, query_only = TRUE)
-  # checkmate::assert_list(result2, min.len = 1)
-  # checkmate::assert_class(result2[[1]], "httr2_request")
+#   # result2 <- mapCompound2CTD(compounds, nParallel = 1, query_only = TRUE)
+#   # checkmate::assert_list(result2, min.len = 1)
+#   # checkmate::assert_class(result2[[1]], "httr2_request")
 
-  # result3 <- mapCompound2CTD(compounds, nParallel = 1, raw = TRUE)
-  # checkmate::assert_list(result3, min.len = 1)
-  # expect_equal(result3[[1]]$class, "Compound")
-  # expect_equal(result3[[1]]$displayName, "Bax channel blocker")
+#   # result3 <- mapCompound2CTD(compounds, nParallel = 1, raw = TRUE)
+#   # checkmate::assert_list(result3, min.len = 1)
+#   # expect_equal(result3[[1]]$class, "Compound")
+#   # expect_equal(result3[[1]]$displayName, "Bax channel blocker")
 
-  # # Test case 3: Invalid compound mapping
-  # compounds <- c("Invalid Compound")
-  # result4 <- mapCompound2CTD(compounds, nParallel = 1)
-  # expect_true("displayName" %in% names(result4))
-  # checkmate::expect_data_table(result4, min.cols = 1, min.rows = 1)
-})
+#   # # Test case 3: Invalid compound mapping
+#   # compounds <- c("Invalid Compound")
+#   # result4 <- mapCompound2CTD(compounds, nParallel = 1)
+#   # expect_true("displayName" %in% names(result4))
+#   # checkmate::expect_data_table(result4, min.cols = 1, min.rows = 1)
+# })
