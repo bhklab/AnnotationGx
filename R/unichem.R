cd0818f8e80cca48dc1a87e79612867f28448e06
Michael Tran <mtran256@uwo.ca>
2025-10-21 11:54:38 -0400

    refactor: retain names for parallel unichem queries

--- R/unichem.R
+++ R/unichem.R
@@ -26,81 +26,81 @@
 #'
 #' @export
 getUnichemSources <- function(all_columns = FALSE) {
-    funContext <- .funContext("AnnotationGx::getUnichemSources")
+  funContext <- .funContext("AnnotationGx::getUnichemSources")
 
-    response <- .build_unichem_query("sources") |>
-        .build_request() |>
-        .perform_request() |>
-        .parse_resp_json()
+  response <- .build_unichem_query("sources") |>
+    .build_request() |>
+    .perform_request() |>
+    .parse_resp_json()
 
-    if (response$response != "Success") {
-        .err(funContext, "Unichem API request failed.")
-    }
+  if (response$response != "Success") {
+    .err(funContext, "Unichem API request failed.")
+  }
 
-    .debug(
-        funContext,
-        sprintf("Unichem sourceCount: %s", response$totalSources)
-    )
+  .debug(
+    funContext,
+    sprintf("Unichem sourceCount: %s", response$totalSources)
+  )
 
-    sources_dt <- .asDT(response$sources)
+  sources_dt <- .asDT(response$sources)
 
-    old_names <- c(
-        "UCICount",
-        "baseIdUrl",
-        "description",
-        "lastUpdated",
-        "name",
-        "nameLabel",
-        "nameLong",
-        "sourceID",
-        "srcDetails",
-        "srcReleaseDate",
-        "srcReleaseNumber",
-        "srcUrl",
-        "updateComments"
-    )
+  old_names <- c(
+    "UCICount",
+    "baseIdUrl",
+    "description",
+    "lastUpdated",
+    "name",
+    "nameLabel",
+    "nameLong",
+    "sourceID",
+    "srcDetails",
+    "srcReleaseDate",
+    "srcReleaseNumber",
+    "srcUrl",
+    "updateComments"
+  )
 
-    new_names <- c(
-        "CompoundCount",
-        "BaseURL",
-        "Description",
-        "LastUpdated",
-        "Name",
-        "NameLabel",
-        "NameLong",
-        "SourceID",
-        "Details",
-        "ReleaseDate",
-        "ReleaseNumber",
-        "URL",
-        "UpdateComments"
-    )
+  new_names <- c(
+    "CompoundCount",
+    "BaseURL",
+    "Description",
+    "LastUpdated",
+    "Name",
+    "NameLabel",
+    "NameLong",
+    "SourceID",
+    "Details",
+    "ReleaseDate",
+    "ReleaseNumber",
+    "URL",
+    "UpdateComments"
+  )
 
-    data.table::setnames(sources_dt, old_names, new_names)
+  data.table::setnames(sources_dt, old_names, new_names)
 
-    new_order <- c(
-        "Name",
-        "NameLabel",
-        "NameLong",
-        "SourceID",
-        "CompoundCount",
-        "BaseURL",
-        "URL",
-        "Details",
-        "Description",
-        "ReleaseNumber",
-        "ReleaseDate",
-        "LastUpdated",
-        "UpdateComments"
-    )
+  new_order <- c(
+    "Name",
+    "NameLabel",
+    "NameLong",
+    "SourceID",
+    "CompoundCount",
+    "BaseURL",
+    "URL",
+    "Details",
+    "Description",
+    "ReleaseNumber",
+    "ReleaseDate",
+    "LastUpdated",
+    "UpdateComments"
+  )
 
-    sources_dt <- sources_dt[, new_order, with = FALSE]
+  sources_dt <- sources_dt[, new_order, with = FALSE]
 
-    if (all_columns) {
-        return(sources_dt)
-    }
+  if (all_columns) {
+    return(sources_dt)
+  }
 
-    sources_dt[, c("Name", "SourceID")]
+  sources_dt[, c("Name", "SourceID")]
 }
 
 #' Query UniChem for a compound.
@@ -131,150 +131,153 @@ getUnichemSources <- function(all_columns = FALSE) {
 #'
 #' @export
 queryUnichemCompound <- function(
-    compound,
-    type,
-    sourceID = NA_integer_,
-    request_only = FALSE,
-    raw = FALSE,
-    progress = "Querying UniChem...",
-    ...
+  compound,
+  type,
+  sourceID = NA_integer_,
+  request_only = FALSE,
+  raw = FALSE,
+  progress = "Querying UniChem...",
+  ...
 ) {
-    checkmate::assert_string(type)
-    checkmate::assert_flag(request_only)
-    checkmate::assert_flag(raw)
-    checkmate::assert(
-        checkmate::check_flag(progress),
-        checkmate::check_string(progress, min.chars = 1)
+  checkmate::assert_string(type)
+  checkmate::assert_flag(request_only)
+  checkmate::assert_flag(raw)
+  checkmate::assert(
+    checkmate::check_flag(progress),
+    checkmate::check_string(progress, min.chars = 1)
+  )
+
+  compounds <- if (is.list(compound)) {
+    unlist(compound, recursive = TRUE, use.names = TRUE)
+  } else {
+    compound
+  }
+  checkmate::assert_atomic_vector(compounds, min.len = 1)
+
+  many_queries <- length(compounds) > 1
+
+  validate_source_ids <- function(src_ids) {
+    if (type != "sourceID") {
+      return(rep(NA_integer_, length(compounds)))
+    }
+
+    checkmate::assert_integerish(src_ids, any.missing = FALSE)
+    total_sources <- max(getUnichemSources()$SourceID)
+
+    if (length(src_ids) == 1L) {
+      src_ids <- rep(src_ids, length(compounds))
+    } else if (length(src_ids) != length(compounds)) {
+      stop(
+        "`sourceID` must be length 1 or match the number of compounds ",
+        "when type = 'sourceID'"
+      )
+    }
+    checkmate::assert_integerish(src_ids, lower = 1, upper = total_sources)
+    src_ids
+  }
+
+  source_ids <- validate_source_ids(sourceID)
+
+  build_request <- function(cmp, src) {
+    .build_unichem_compound_req(
+      type = type,
+      compound = cmp,
+      sourceID = if (is.na(src)) NULL else src,
+      ...
+    )
+  }
+
+  parse_response <- function(parsed, cmp_label) {
+    if (parsed$response != "Success") {
+      msg <- paste(
+        "Unichem API request failed for compound",
+        cmp_label,
+        "with type",
+        type,
+        ". Error:",
+        parsed$error
+      )
+      .err(.funContext("AnnotationGx::queryUnichemCompound"), msg)
+    }
+
+    mapped_sources_dt <- .asDT(parsed$compounds$sources)
+    old_names <- c("compoundId", "shortName", "longName", "id", "url")
+    new_names <- c(
+      "compoundID",
+      "Name",
+      "NameLong",
+      "sourceID",
+      "sourceURL"
+    )
+    data.table::setnames(
+      mapped_sources_dt,
+      old = old_names,
+      new = new_names
     )
 
-    compounds <- if (is.list(compound)) {
-        unlist(compound, recursive = TRUE, use.names = FALSE)
+    External_Mappings <- mapped_sources_dt[, new_names, with = FALSE]
+
+    UniChem_Mappings <- list(
+      UniChem.UCI = parsed$compounds$uci,
+      UniChem.InchiKey = parsed$compounds$standardInchiKey,
+      UniChem.Inchi = parsed$compounds$inchi$inchi,
+      UniChem.formula = parsed$compounds$inchi$formula,
+      UniChem.connections = parsed$compounds$inchi$connections,
+      UniChem.hAtoms = parsed$compounds$inchi$hAtoms
+    )
+
+    list(
+      External_Mappings = External_Mappings,
+      UniChem_Mappings = UniChem_Mappings
+    )
+  }
+
+  if (many_queries) {
+    requests <- Map(build_request, compounds, source_ids)
+    name_candidates <- names(compounds)
+    if (
+      !is.null(name_candidates) && length(name_candidates) == length(compounds)
+    ) {
+      names(requests) <- name_candidates
     } else {
-        compound
-    }
-    checkmate::assert_atomic_vector(compounds, min.len = 1)
-
-    many_queries <- length(compounds) > 1
-
-    validate_source_ids <- function(src_ids) {
-        if (type != "sourceID") {
-            return(rep(NA_integer_, length(compounds)))
-        }
-
-        checkmate::assert_integerish(src_ids, any.missing = FALSE)
-        total_sources <- max(getUnichemSources()$SourceID)
-
-        if (length(src_ids) == 1L) {
-            src_ids <- rep(src_ids, length(compounds))
-        } else if (length(src_ids) != length(compounds)) {
-            stop(
-                "`sourceID` must be length 1 or match the number of compounds ",
-                "when type = 'sourceID'"
-            )
-        }
-        checkmate::assert_integerish(src_ids, lower = 1, upper = total_sources)
-        src_ids
+      names(requests) <- as.character(compounds)
     }
 
-    source_ids <- validate_source_ids(sourceID)
-
-    build_request <- function(cmp, src) {
-        .build_unichem_compound_req(
-            type = type,
-            compound = cmp,
-            sourceID = if (is.na(src)) NULL else src,
-            ...
-        )
-    }
-
-    parse_response <- function(parsed, cmp_label) {
-        if (parsed$response != "Success") {
-            msg <- paste(
-                "Unichem API request failed for compound",
-                cmp_label,
-                "with type",
-                type,
-                ". Error:",
-                parsed$error
-            )
-            .err(.funContext("AnnotationGx::queryUnichemCompound"), msg)
-        }
-
-        mapped_sources_dt <- .asDT(parsed$compounds$sources)
-        old_names <- c("compoundId", "shortName", "longName", "id", "url")
-        new_names <- c(
-            "compoundID",
-            "Name",
-            "NameLong",
-            "sourceID",
-            "sourceURL"
-        )
-        data.table::setnames(
-            mapped_sources_dt,
-            old = old_names,
-            new = new_names
-        )
-
-        External_Mappings <- mapped_sources_dt[, new_names, with = FALSE]
-
-        UniChem_Mappings <- list(
-            UniChem.UCI = parsed$compounds$uci,
-            UniChem.InchiKey = parsed$compounds$standardInchiKey,
-            UniChem.Inchi = parsed$compounds$inchi$inchi,
-            UniChem.formula = parsed$compounds$inchi$formula,
-            UniChem.connections = parsed$compounds$inchi$connections,
-            UniChem.hAtoms = parsed$compounds$inchi$hAtoms
-        )
-
-        list(
-            External_Mappings = External_Mappings,
-            UniChem_Mappings = UniChem_Mappings
-        )
-    }
-
-    if (many_queries) {
-        requests <- Map(build_request, compounds, source_ids)
-        names(requests) <- if (!is.null(names(compound))) {
-            names(compound)
-        } else {
-            as.character(compounds)
-        }
-
-        if (request_only) {
-            return(requests)
-        }
-
-        responses <- .perform_request_parallel(requests, progress = progress)
-        names(responses) <- names(requests)
-
-        parsed_responses <- Map(.parse_resp_json, responses)
-
-        if (raw) {
-            names(parsed_responses) <- names(responses)
-            return(parsed_responses)
-        }
-
-        results <- Map(
-            parse_response,
-            parsed_responses,
-            names(responses)
-        )
-
-        names(results) <- names(responses)
-        return(results)
-    }
-
-    request <- build_request(compounds[[1L]], source_ids[[1L]])
     if (request_only) {
-        return(request)
+      return(requests)
     }
 
-    response <- request |> .perform_request()
-    parsed <- .parse_resp_json(response)
+    responses <- .perform_request_parallel(requests, progress = progress)
+    names(responses) <- names(requests)
+
+    parsed_responses <- Map(.parse_resp_json, responses)
 
     if (raw) {
-        return(parsed)
+      names(parsed_responses) <- names(responses)
+      return(parsed_responses)
     }
 
-    parse_response(parsed, compounds[[1L]])
+    results <- Map(
+      parse_response,
+      parsed_responses,
+      names(responses)
+    )
+
+    names(results) <- names(responses)
+    return(results)
+  }
+
+  request <- build_request(compounds[[1L]], source_ids[[1L]])
+  if (request_only) {
+    return(request)
+  }
+
+  response <- request |> .perform_request()
+  parsed <- .parse_resp_json(response)
+
+  if (raw) {
+    return(parsed)
+  }
+
+  parse_response(parsed, compounds[[1L]])
 }
